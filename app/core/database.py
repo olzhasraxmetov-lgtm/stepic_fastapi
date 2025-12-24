945ebd629fea5e3427d658b6a9437e0b32a6ca9d
Olzhas Rakhmetov <olzhasraxmetov@gmail.com>
2025-12-24 18:25:04 +0500

    fix: rename the parametr for create_async_engine

---
+++ app/core/database.py
@@ -0,0 +1,14 @@
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.orm import DeclarativeBase
+from app.core.config import config
+
+
+async_engine = create_async_engine(url=config.DATABASE_URL,echo=True)
+AsyncSessionLocal = async_sessionmaker(
+    bind=async_engine,
+    expire_on_commit=False,
+    class_=AsyncSession,
+)
+
+class Base(DeclarativeBase):
+    pass

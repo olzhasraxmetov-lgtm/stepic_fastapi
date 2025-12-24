64c200db858580114d0c876a1e191af63c4d46b2
Olzhas Rakhmetov <olzhasraxmetov@gmail.com>
2025-12-24 19:07:43 +0500

    feat: add lifespan

--- app/main.py
+++ app/main.py
@@ -1,11 +1,17 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 from app.core.config import config
-
 app = FastAPI(
     title=config.APP_NAME,
     description=config.APP_DESCRIPTION,
     version=config.APP_VERSION
 )
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print('App is starting')
+    yield
+    print('App is stopping')
 
 @app.get('/')
 async def root():

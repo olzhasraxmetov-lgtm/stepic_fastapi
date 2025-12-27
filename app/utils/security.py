33a5db81495266a6e1fa3e2088b5990bbf775890
Olzhas Rakhmetov <olzhasraxmetov@gmail.com>
2025-12-27 17:42:37 +0500

    feat(security): add security utility methods

---
+++ app/utils/security.py
@@ -0,0 +1,26 @@
+from passlib.context import CryptContext
+from datetime import datetime, timedelta, timezone
+import jwt
+from app.core.config import config
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def hash_password(password: str) -> str:
+    """Хеширует пароль"""
+    return pwd_context.hash(password)
+
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    """Проверяет пароль"""
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def create_access_token(data: dict) -> str:
+    """
+    Создаёт JWT токен с payload (sub, username, id, exp).
+    """
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(minutes=config.ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, config.SECRET_KEY, algorithm=config.ALGORITHM)

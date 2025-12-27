5d964385da631d206f103e47ba0365f572cf3ee2
Olzhas Rakhmetov <olzhasraxmetov@gmail.com>
2025-12-27 18:54:31 +0500

    feat(router): complete user registration endpoint

--- app/api/v1/user.py
+++ app/api/v1/user.py
@@ -1,5 +1,9 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from starlette import status
 
+from app.schemas.user import UserCreate, UserResponse
+from app.services.user import UserService
+from app.core.dependencies import get_user_service
 
 user_router = APIRouter(
     prefix="/user",
@@ -7,6 +11,9 @@ user_router = APIRouter(
 )
 
 
-@user_router.post("/register")
-async def register_user():
-    pass
+@user_router.post("/register", status_code=status.HTTP_201_CREATED, response_model=UserResponse)
+async def register_user(
+        user: UserCreate,
+        user_service: UserService = Depends(get_user_service)
+):
+    return await user_service.register(user)

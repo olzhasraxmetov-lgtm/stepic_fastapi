900bd2c828381a9e080abda6051a28a8d29001fe
Olzhas Rakhmetov <olzhasraxmetov@gmail.com>
2025-12-27 17:15:47 +0500

    feat(dependencies): add user repository and service providers

--- app/core/dependencies.py
+++ app/core/dependencies.py
@@ -2,6 +2,19 @@ from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
 from app.core.database import AsyncSessionLocal
 from collections.abc import AsyncGenerator
 
+from app.services.user import UserService
+from app.repositories.user import UserRepository
+from fastapi import Depends
+
+
+
 async def get_db() -> AsyncGenerator[AsyncSession, None]:
     async with AsyncSessionLocal() as session:
         yield session
+
+async def get_user_repository(db: AsyncSession = Depends(get_db)) -> UserRepository:
+    return UserRepository(session=db)
+
+
+async def get_user_service(repository: UserRepository = Depends(get_user_repository)) -> UserService:
+    return UserService(repository=repository)

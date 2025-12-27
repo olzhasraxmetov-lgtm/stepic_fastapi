ed905c8778de7d0874caa17a4392d6c994a512b2
Olzhas Rakhmetov <olzhasraxmetov@gmail.com>
2025-12-27 19:17:43 +0500

    feat(service): add user login method

--- app/services/user.py
+++ app/services/user.py
@@ -5,7 +5,7 @@ from app.repositories.user import UserRepository
 from app.schemas.user import UserCreate, UserResponse
 from app.models.user import UserORM
 
-from app.utils.security import hash_password
+from app.utils.security import hash_password, verify_password, create_access_token
 
 class UserService:
     def __init__(self, repository: UserRepository):
@@ -27,3 +27,21 @@ class UserService:
         )
         created_user = await self.repository.create(db_user)
         return UserResponse.model_validate(created_user)
+
+    async def login(self, user_name: str, password: str) -> dict:
+        user = await self.repository.get_by_username(user_name)
+        if user is None:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Incorrect username or password"
+            )
+
+        if not verify_password(password, user.hashed_password):
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Incorrect username or password"
+            )
+        access_token = create_access_token(
+            data={"sub": user.email, "username": user.username, "id": user.id},
+        )
+        return {"access_token": access_token, "token_type": "bearer"}

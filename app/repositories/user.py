3ade025527c925640744884076253abd228d4be1
Olzhas Rakhmetov <olzhasraxmetov@gmail.com>
2025-12-27 17:35:59 +0500

    feat(repository): add get_by_email method

--- app/repositories/user.py
+++ app/repositories/user.py
@@ -1,9 +1,11 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from pydantic import EmailStr
 from app.repositories.base import BaseRepository
 from app.models.user import UserORM
 
 from sqlalchemy import select
+from app.schemas.user import UserCreate, UserResponse
 
 class UserRepository(BaseRepository):
     def __init__(self, session: AsyncSession):
@@ -12,5 +14,11 @@ class UserRepository(BaseRepository):
     async def get_by_username(self, username: str) -> UserORM | None:
         result = await self.session.scalars(
             select(UserORM).where(UserORM.username == username)
+        )
+        return result.first()
+
+    async def get_by_email(self, email: EmailStr) -> UserORM | None:
+        result = await self.session.scalars(
+            select(UserORM).where(UserORM.email == email)
         )
         return result.first()

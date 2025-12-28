a8e8c5c7239c13ad2398f1d4fa81fd0a80e74e71
Olzhas Rakhmetov <olzhasraxmetov@gmail.com>
2025-12-28 19:58:19 +0500

    chore(database): change echo parameter for create_async_engine function

--- app/core/database.py
+++ app/core/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import DeclarativeBase
 
 from app.core.config import config
 
-async_engine = create_async_engine(url=config.DATABASE_URL,echo=True)
+async_engine = create_async_engine(url=config.DATABASE_URL,echo=False)
 AsyncSessionLocal = async_sessionmaker(
     bind=async_engine,
     expire_on_commit=False,

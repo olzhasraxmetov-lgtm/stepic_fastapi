8c0d3453f55177c990c68fb8673d4bb191b97760
Olzhas Rakhmetov <olzhasraxmetov@gmail.com>
2026-01-04 19:22:02 +0500

    feat(course): implement create course logic - add POST /course endpoint - implement create_course in service with AUTHOR/ADMIN role check

--- app/api/v1/course.py
+++ app/api/v1/course.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter, Depends
 from starlette import status
 
-from app.core.dependencies import get_current_user
 from app.core.dependencies import get_course_service
-from app.models.course import CourseORM
-
+from app.core.dependencies import get_current_user
+from app.models.user import UserORM
+from app.schemas.course import CourseResponse, CourseCreate
 from app.services.course import CourseService
 
 course_router = APIRouter(
@@ -15,3 +15,12 @@ course_router = APIRouter(
 @course_router.get('/')
 async def get_courses():
     return 'Courses'
+
+
+@course_router.post('/', status_code=status.HTTP_201_CREATED, response_model=CourseResponse)
+async def create_course(
+        payload: CourseCreate,
+        user: UserORM = Depends(get_current_user),
+        course_service: CourseService = Depends(get_course_service),
+):
+    return await course_service.create_course(user, payload)
--- app/services/course.py
+++ app/services/course.py
@@ -2,12 +2,34 @@ from loguru import logger
 
 from app.core.exceptions import NotFoundException, ConflictException, UnauthorizedException, ForbiddenException, \
     BaseAppException
-
+from app.helpers.user_role import UserRoleEnum
 from app.models.course import CourseORM
-
+from app.models.user import UserORM
+from app.schemas.course import CourseCreate, CourseResponse
 from app.repositories.course import CourseRepository
 
 class CourseService:
 
     def __init__(self, course_repo: CourseRepository):
         self.course_repo = course_repo
+
+    async def create_course(self, current_user: UserORM, payload: CourseCreate) -> CourseResponse:
+        logger.debug(f'Attempting to create a course by user: {current_user.id}')
+        if current_user.role not in [UserRoleEnum.AUTHOR, UserRoleEnum.ADMIN]:
+            logger.warning(f'Failed to create a course by user:  {current_user.id}')
+            raise ForbiddenException(
+                message='Only authors can create courses',
+                log_message=f'User {current_user.id} with role {current_user.role} tried to create course'
+            )
+        created_course = payload.model_dump()
+        created_course['author_id'] = current_user.id
+
+        try:
+            created_course = await self.course_repo.create(created_course)
+            logger.success(f'Successfully created {payload.title} by user: {current_user.id}')
+            return CourseResponse.model_validate(created_course)
+        except Exception as e:
+            logger.exception(f"Database error while creating course '{payload.title}': {e}")
+        raise
+
+

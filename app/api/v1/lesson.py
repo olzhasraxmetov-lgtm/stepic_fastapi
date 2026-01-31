7282883f3c19d7288541a8ad0971eb059372c106
Olzhas Rakhmetov <olzhasraxmetov@gmail.com>
2026-01-31 16:51:30 +0500

    feat(lesson): optimize imports and integrate course and user dependencies into lesson endpoints

--- app/api/v1/lesson.py
+++ app/api/v1/lesson.py
@@ -1,17 +1,21 @@
-from fastapi import APIRouter, Depends, Query
+from typing import Annotated
+
+from fastapi import APIRouter, Depends
 from starlette import status
 
+from app.api.v1.step import step_router
+from app.core.dependencies import get_course_with_access
 from app.core.dependencies import get_lesson_service
-from app.core.dependencies import get_current_user
+from app.core.dependencies import valid_lesson
+from app.core.dependencies import validation_course_id
+from app.models.course import CourseORM
 from app.models.lesson import LessonORM
 from app.schemas.lesson import LessonCreate, LessonUpdate, LessonResponse
 from app.services.lesson import LessonService
-from app.models.user import UserORM
-from app.core.dependencies import validation_course_id
-from app.models.course import CourseORM
-from app.core.dependencies import get_course_with_access
+
 lesson_router = APIRouter(
     prefix="/{course_id}/lessons",
+    dependencies=[Depends(validation_course_id)],
 )
 
 
@@ -26,25 +30,27 @@ async def get_lessons(
 @lesson_router.post('/', tags=["Lessons"])
 async def create_lesson(
         payload: LessonCreate,
-        course: CourseORM = Depends(validation_course_id),
+        course: CourseORM = Depends(get_course_with_access),
         lessons_service: LessonService = Depends(get_lesson_service),
-        current_user: UserORM = Depends(get_current_user)
 ):
-    return await lessons_service.create_lesson(payload=payload, current_user=current_user, course=course)
+    return await lessons_service.create_lesson(payload=payload, course=course)
 
 @lesson_router.patch('/{lesson_id}', tags=["Lessons"], response_model=LessonResponse)
 async def update_lesson(
-        lesson_id: int,
         payload: LessonUpdate,
-        course: CourseORM = Depends(get_course_with_access),
-        lesson_service: LessonService = Depends(get_lesson_service),
+        course: Annotated[CourseORM, Depends(get_course_with_access)],
+        lesson: Annotated[LessonORM, Depends(valid_lesson)],
+        lesson_service: Annotated[LessonService, Depends(get_lesson_service)],
 ):
-    return await lesson_service.update_lesson(payload=payload, course=course, lesson_id=lesson_id)
+    return await lesson_service.update_lesson(payload=payload, lesson=lesson)
 
 @lesson_router.delete('/{lesson_id}', tags=["Lessons"], status_code=status.HTTP_200_OK)
 async def delete_lesson(
         lesson_id: int,
-        course: CourseORM = Depends(get_course_with_access),
+        course: Annotated[CourseORM, Depends(get_course_with_access)],
+        lesson: Annotated[LessonORM, Depends(valid_lesson)],
         lesson_service: LessonService = Depends(get_lesson_service),
 ):
-    return await lesson_service.delete_lesson(course=course, lesson_id=lesson_id)
+    return await lesson_service.delete_lesson(lesson=lesson)
+
+lesson_router.include_router(step_router)

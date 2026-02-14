45d170d31c7fed8e9dd0ab25f72d49e8b81c08e8
Olzhas Rakhmetov <olzhasraxmetov@gmail.com>
2026-02-14 20:09:56 +0500

    feat(schema): update schemas related to Comments for better reading

--- app/schemas/comment.py
+++ app/schemas/comment.py
@@ -2,38 +2,43 @@ from pydantic import BaseModel, ConfigDict, Field
 from datetime import datetime
 from decimal import Decimal
 
-class CommentBase(BaseModel):
-    content: str = Field(..., min_length=1, max_length=3000, description='Содержимое комментария')
-
-class CommentCreate(CommentBase):
-    pass
-
-class CommentReplyCreate(CommentCreate):
-    parent_id: int
-
-class CommentUpdate(BaseModel):
-    content: str | None = None
-
 class CommentAuthor(BaseModel):
     id: int
     username: str
 
     model_config = ConfigDict(from_attributes=True)
 
-class CommentResponse(CommentBase):
+class CommentBase(BaseModel):
+    content: str = Field(..., min_length=1, max_length=3000, description='Содержимое комментария')
     id: int
-
-    parent_id: int | None = None
-    step_title: str | None = None
     created_at: datetime
-    updated_at: datetime
     author: CommentAuthor
-    is_edited: bool = False
     is_deleted: bool = False
-    children: list["CommentResponse"] = []
-
     model_config = ConfigDict(from_attributes=True)
 
+class CommentCreate(BaseModel):
+    content: str = Field(..., min_length=1, max_length=3000, description='Содержимое комментария')
 
+class CommentUpdate(BaseModel):
+    content: str | None = None
 
-CommentResponse.model_rebuild()
+class CommentShortResponse(CommentBase):
+    step_title: str | None = None
+    parent_id: int | None = None
+    is_edited: bool = False
+    likes_count: int = 0
+    dislikes_count: int = 0
+
+class CommentFullResponse(CommentBase):
+    parent_id: int | None = None
+    step_title: str | None = None
+    is_edited: bool = False
+    updated_at: datetime
+
+    likes_count: int = 0
+    dislikes_count: int = 0
+    is_liked_by_me: bool = False
+    is_disliked_by_me: bool = False
+    children: list["CommentFullResponse"] = []
+
+CommentFullResponse.model_rebuild()

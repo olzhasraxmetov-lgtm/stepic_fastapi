7c54633f18807e702d95a307f002aae528c4d62b
Olzhas Rakhmetov <olzhasraxmetov@gmail.com>
2026-02-14 18:04:56 +0500

    refactor: rename repository parametr to reaction_repository

--- app/services/reaction.py
+++ app/services/reaction.py
@@ -1,6 +1,7 @@
 from app.repositories.reaction import ReactionRepository
-
+from app.services.comment import CommentService
 
 class ReactionService:
-    def __init__(self, repository: ReactionRepository):
-        self.repository = repository
+    def __init__(self, reaction_repository: ReactionRepository, comment_service: CommentService):
+        self.reaction_repository = reaction_repository
+        self.comment_service = comment_service
